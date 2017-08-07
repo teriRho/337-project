85e091dd09ba13949bbfa9464cbb612c2d1901f8
teriRho <trhodes@oakland.edu>
2017-08-07 11:15:30 -0400

    Update TicTacToeManger.java

--- source/edu/oakland/production/manager/TicTacToeManger.java
+++ source/edu/oakland/production/manager/TicTacToeManger.java
@@ -42,7 +42,7 @@ while(this.gameIsOver == false){
 	display.printCurrentBoard();
 	
 	// giving display current team, 
-	display.chooseSquare(this.currentplayer);
+	display.placeMarkOnBoard(this.currentplayer);
 	
 	// Checking if game is over
 	this.gameIsOver = display.checkForWinOrDraw();
